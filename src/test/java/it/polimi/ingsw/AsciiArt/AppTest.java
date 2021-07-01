8b6add9cce218490942ecd55ac5bd112ad18ff80
ing.conti <ingconti@ingconti.com>
2021-07-01 08:05:25 +0200

    fixed package. fixed buiding JAR with shade

---
+++ src/test/java/it/polimi/ingsw/AsciiArt/AppTest.java
@@ -0,0 +1,18 @@
+package AsciiArt;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
